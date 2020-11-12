c50823fe9f1cc15db7c35951a1e39e5a0162cf6b
it-mikhail <mikhail.tupitsyn@gmail.com>
2020-11-12 18:07:25 +0300

    finished task. ready to check

---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,10 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+public class Cat extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
